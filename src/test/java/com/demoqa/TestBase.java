65df807c22c46c02d6b58bfbd4d8a3bb6268b8d3
Natalia Sinitsyna <natalia.a.sinitsyna@gmail.com>
2023-05-09 23:12:31 +0300

    add test for Student Registration Form

---
+++ src/test/java/com/demoqa/TestBase.java
@@ -0,0 +1,16 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import com.codeborne.selenide.Configuration;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+
+    }
+}
